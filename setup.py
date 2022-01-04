59e72ccfedb61bdf833e75b55e0ade733653601f
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2022-01-04 23:14:35 +0100

    Update the grafana dashboard templater (#1)
    * Add unittests
    * Add/ Update GitHub actions
    * Add docstrings
    * Add dashboard example
    * Update tests, src, documentation, github actions and add setup.py
    * Update the documentation and Github actions
    * Update the tests and setup.py
    * Update the README.md
    * Add coverage badge

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grafana-dashboard-templater",
+    version="0.0.1",
+    author="Pascal Zimmermann",
+    author_email="info@theiotstudio.com",
+    description="A Grafana dashboard templater",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZPascal/grafana_dashboard_templater",
+    project_urls={
+        "Bug Tracker": "https://github.com/ZPascal/grafana_dashboard_templater/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=["jinja2"],
+    python_requires=">=3.6",
+)
