89d688b1aad0b4b202a86cc6f14391b43b6ab68d
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2022-01-05 08:36:05 +0100

    Update the used license configuration

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
